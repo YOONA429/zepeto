cc6af29fa44c2508ec33b58697241c8cf328322b
YOONA KIM <dool42962@gmail.com>
2024-08-19 11:14:37 +0900

    Add files via upload

--- scripts/common.js
+++ scripts/common.js
@@ -10,3 +10,41 @@ const gnbClone = gnb.cloneNode(true)
 console.log(gnb, mNav, gnbClone)
 mNav.appendChild(gnbClone)
 //복사한거 붙여놓을 위치 마지막에 복제한 내용을 복사할거라는 뜻
+
+//main-nav
+//sub,sub_bg 숨기기
+$('nav .sub').hide()
+$('nav .sub_bg').hide()
+
+//nav 마우스 오버 시 sub 보이기
+$('nav').on('mouseover',function(){
+    $('nav .sub').stop(true).slideDown(300)
+    $('nav .sub_bg').stop(true).slideDown(300)
+    //$('nav .sub').show()
+    //$('nav .sub_bg').show()
+})
+$('nav').on('mouseout',function(){
+    $('nav .sub').stop(true).slideUp(300)
+    $('nav .sub_bg').stop(true).slideUp(300)
+})
+
+// mobile menu
+$('.m_nav_wrap').hide();
+//모바일 메뉴의 현재상태를 알기위한 논리형 데이터생성
+var m_nav_state = true;
+
+
+//전체메뉴 아이콘 클릭 시
+/* $('.m_nav').on('click',function(){
+    m_nav_state = !m_nav_state
+    console.log(`현재 상태는 : ${m_nav_state}`)
+    if(m_nav_state == false) {
+        $('.m_nav_wrap').show();
+        m_nav_state = true //상태변경
+    }else{
+        $('.m_nav_wrap').hide();
+        //m_nav_state = false
+    }
+}) */
+//(모바일메뉴가 숨겨진 상태라면) 보이기
+//(모바일메뉴가 보이는 상태라면) 보이기
